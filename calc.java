2807d0f7cda503a1c6ceeddf5927c66ffe3ebbd0
Krishna5006 <b.krishnaworks@gmail.com>
2025-01-27 22:55:26 +0530

    hello

---
+++ calc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        System.out.print("Enter First number");
+        a=sc.nextInt();
+        System.out.print("Enter Second number");
+        b=sc.nextInt();
+    }
+}
