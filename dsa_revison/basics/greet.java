21b3bdbd4403b91db77e352bd28e5f666a8473a2
Krishna5006 <b.krishnaworks@gmail.com>
2025-09-06 17:52:54 +0530

    Dsa started

---
+++ dsa_revison/basics/greet.java
@@ -0,0 +1,14 @@
+package dsa_revison.basics;
+
+import java.util.Scanner;
+public class greet {
+    public static void main(String[] args) 
+    {
+        System.out.print("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("Hello " +name +" How are you?");
+        sc.close();
+    }
+
+}
