7c9fe02ff42149ff247e8a7ecd33c71ab2b75720
Krishna5006 <b.krishnaworks@gmail.com>
2025-09-06 18:00:41 +0530

    updated file location with corresponding folder name

---
+++ dsa_revison/basics/intro_printing/hello.java
@@ -0,0 +1,9 @@
+package dsa_revison.basics.intro_printing;
+
+public class hello 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");        
+    }
+    
+}
