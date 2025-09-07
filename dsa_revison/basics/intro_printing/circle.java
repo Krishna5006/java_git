8594b3544e8ec8db1ccddea2883cd841cf5d495d
Krishna5006 <b.krishnaworks@gmail.com>
2025-09-07 23:36:49 +0530

    Circle area and circumference

---
+++ dsa_revison/basics/intro_printing/circle.java
@@ -0,0 +1,15 @@
+package dsa_revison.basics.intro_printing;
+
+import java.util.Scanner;
+public class circle {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int radius;
+        System.out.println("Enter Radius");
+        radius=sc.nextInt();
+        System.out.println("Area of Circle is: ");
+        
+
+    }
+}
