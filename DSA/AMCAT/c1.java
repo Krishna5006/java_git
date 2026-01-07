5873cc2e373c0dc111245374452e737f71aa01c8
Krishna Bisht <b.krishnaworks@gmail.com>
2026-01-07 23:20:53 +0530

    AMCAT code

--- DSA/AMCAT/c1.java
+++ DSA/AMCAT/c1.java
@@ -1,5 +1,24 @@
 package DSA.AMCAT;
-
-public class c1{
-    
+import java.util.*;
+public class c1
+{
+   public static void main(String[] args) 
+   {
+    Scanner sc=new Scanner(System.in);
+    System.err.print("Enter number:");
+    int num=sc.nextInt();
+    EvenOddCheck(num);
+    sc.close();
+   } 
+   public static void EvenOddCheck(int num)
+   {
+        if(num%2==0)
+        {
+            System.err.println("Number is even:"+ num);
+        }
+        else
+        {
+            System.err.println("Number is odd:"+ num);
+        }
+    }
 }
