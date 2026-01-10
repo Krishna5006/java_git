44ea4bc17b28ec2d1f3ba45a0d6f07868e7b4e35
Krishna Bisht <b.krishnaworks@gmail.com>
2026-01-10 23:27:10 +0530

    ceiling and floor problem

---
+++ DSA/4.BinarySearch/Questions/CeilingProblem.java
@@ -0,0 +1,48 @@
+package Questions;
+
+public class CeilingProblem {
+    public static void main(String[] args) {
+        int[] arr={2,3,5,9,14,16,18};
+        int target=15;
+        System.out.println(CeilingProblemFunction(arr, target));
+    }
+    static int CeilingProblemFunction(int[] arr,int target){
+
+        //but what if the target is greater than the greatest no in the array
+        if (target > arr[arr.length-1])
+        {
+            return -1;
+        }
+        
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return start;
+    }
+}
